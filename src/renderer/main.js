edc7a835e18f1d3ac9d478c574e1762909e8e7f2
leejooy96 <leejooy96@gmail.com>
2022-06-14 09:10:56 +0900

    renderer: add pinia package

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App'
 import router from './router'
 import vuetify from './plugins/vuetify'
+
 import 'vuetify/dist/vuetify.min.css'
 
 const app = createApp(App)
@@ -10,5 +12,6 @@ const app = createApp(App)
 app
   .use(vuetify)
   .use(router)
+  .use(createPinia())
 
 app.mount('#app')
