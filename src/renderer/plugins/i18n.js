ab49931b450760664efe49fcbc083e9e51ab48ed
leejooy96 <leejooy96@gmail.com>
2022-06-29 09:02:36 +0900

    renderer: remove unused eslint disable line

--- src/renderer/plugins/i18n.js
+++ src/renderer/plugins/i18n.js
@@ -3,7 +3,6 @@ import en from '../../locales/en.json'
 import ko from '../../locales/ko.json'
 import Utils from '../assets/js/utils'
 
-// eslint-disable-next-line new-cap
 export default createI18n({
   legacy: false,
   locale: Utils.getCurrentLocale(),
