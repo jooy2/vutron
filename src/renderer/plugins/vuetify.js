cd1d9d3a04a1c1b423656e5f9f95a1298c8e10dd
leejooy96 <leejooy96@gmail.com>
2022-06-30 09:22:38 +0900

    renderer: add vuetify localization

--- src/renderer/plugins/vuetify.js
+++ src/renderer/plugins/vuetify.js
@@ -1,7 +1,13 @@
 import { createVuetify } from 'vuetify/dist/vuetify'
+import { ko, en } from 'vuetify/locale'
 
 export default createVuetify({
   icons: {
     iconfont: 'mdi'
+  },
+  locale: {
+    messages: { ko, en },
+    defaultLocale: 'en',
+    fallbackLocale: 'en'
   }
 })
