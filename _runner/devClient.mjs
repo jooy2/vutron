432a49bdf0d23d416ba6f4d9865b103895f84544
leejooy96 <leejooy96@gmail.com>
2022-06-30 09:30:34 +0900

    pack: minor typo fix

--- _runner/devClient.mjs
+++ _runner/devClient.mjs
@@ -6,7 +6,7 @@ hotClient.subscribe(event => {
       <style>
         .root {
           color: #000;
-          padding: 10px 20px;
+          padding: 10px 20px
         }
       </style>
       <div class="root">Compiling Main Process...</div>
