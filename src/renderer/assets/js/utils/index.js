df3906c1cc00abcc3cfec729ececf8d755add6e8
leejooy96 <leejooy96@gmail.com>
2022-07-12 09:24:25 +0900

    renderer: add theme switcher, go to about page button in main screen

--- src/renderer/assets/js/utils/index.js
+++ src/renderer/assets/js/utils/index.js
@@ -1,5 +1,11 @@
+import { shell } from 'electron'
+
 export default class Utils {
   static getCurrentLocale () {
     return navigator?.language?.split('-')[0] || 'en'
+  }
+
+  static async openExternal (url) {
+    await shell.openExternal(url)
   }
 }
--- src/renderer/components/screen/MainScreen.vue
+++ src/renderer/components/screen/MainScreen.vue
@@ -1,4 +1,22 @@
 <script setup>
+// import { useI18n } from 'vue-i18n'
+import { useTheme } from 'vuetify'
+import Utils from '../../assets/js/utils'
+
+// const { locale } = useI18n()
+const theme = useTheme()
+
+const handleChangeTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+const handleChangeLanguage = () => {
+  // locale.value = 'en' // TODO
+}
+
+const handleAbout = async () => {
+  await Utils.openExternal('https://github.com/jooy2/vutron')
+}
 </script>
 
 <template>
@@ -16,6 +34,34 @@
       </v-col>
       <v-col cols="12">
         {{ $t('desc.welcome') }}
+      </v-col>
+      <v-col cols="4">
+        <v-btn
+          block
+          color="primary"
+          @click="handleChangeTheme"
+        >
+          {{ $t('menu.change-theme') }}
+        </v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-btn
+          block
+          disabled
+          color="primary"
+          @click="handleChangeLanguage"
+        >
+          {{ $t('menu.change-language') }}
+        </v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-btn
+          block
+          color="primary"
+          @click="handleAbout"
+        >
+          {{ $t('menu.about') }}
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
