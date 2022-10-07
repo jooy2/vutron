d274dee0d7640327ac4c0bfb64826f6f6457764e
leejooy96 <leejooy96@gmail.com>
2022-10-07 16:15:47 +0900

    pack: remove i18n warning message

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import { resolve, join } from 'path'
 import { builtinModules } from 'module'
 
 export default defineConfig({
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false
+  },
   resolve: {
     extensions: [
       '.mjs',
