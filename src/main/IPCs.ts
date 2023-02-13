e4b3b8585a735f9d3547082ab4b7722394e42a54
leejooy96 <leejooy96@gmail.com>
2023-02-13 18:26:19 +0900

    main: add missing types

--- src/main/IPCs.ts
+++ src/main/IPCs.ts
@@ -5,14 +5,14 @@ import Constants from './utils/Constants'
  * IPC Communications
  * */
 export default class IPCs {
-  static initialize(window: BrowserWindow) {
+  static initialize(window: BrowserWindow): void {
     // Get application version
     ipcMain.on('msgRequestGetVersion', () => {
       window.webContents.send('msgReceivedVersion', Constants.APP_VERSION)
     })
 
     // Open url via web browser
-    ipcMain.on('msgOpenExternalLink', async (event, url) => {
+    ipcMain.on('msgOpenExternalLink', async (event, url: string) => {
       await shell.openExternal(url)
     })
   }
