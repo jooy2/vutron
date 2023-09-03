8204250f74c8e9dff4f025eef4e5c284d07efe1f
leejooy96 <leejooy96@gmail.com>
2023-09-03 12:16:16 +0900

    pack: install vue devtools extension

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,11 +2,19 @@ import { app, WebContents, RenderProcessGoneDetails } from 'electron'
 import Constants from './utils/Constants'
 import { createErrorWindow, createMainWindow } from './MainRunner'
 import { macOSDisableDefaultMenuItem } from './utils/Menus'
+import { installExtension, VUEJS_DEVTOOLS } from 'electron-extension-installer'
 
 let mainWindow
 let errorWindow
 
 app.on('ready', () => {
+  if (Constants.IS_DEV_ENV) {
+    installExtension(VUEJS_DEVTOOLS, {
+      loadExtensionOptions: {
+        allowFileAccess: true
+      }
+    })
+  }
   macOSDisableDefaultMenuItem()
 
   mainWindow = createMainWindow(mainWindow)
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
-import electronPlugin from 'vite-plugin-electron'
-import rendererPlugin from 'vite-plugin-electron-renderer'
-import eslintPlugin from 'vite-plugin-eslint'
-import vuetifyPlugin from 'vite-plugin-vuetify'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vue from '@vitejs/plugin-vue'
+import ElectronPlugin from 'vite-plugin-electron'
+import RendererPlugin from 'vite-plugin-electron-renderer'
+import EslintPlugin from 'vite-plugin-eslint'
+import VuetifyPlugin from 'vite-plugin-vuetify'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import Vue from '@vitejs/plugin-vue'
 import { rmSync } from 'fs'
 import { resolve, dirname } from 'path'
 import { builtinModules } from 'module'
@@ -34,16 +34,16 @@ export default defineConfig(() => {
       outDir: resolve('./dist')
     },
     plugins: [
-      vue(),
-      vueJsx(),
+      Vue(),
+      VueJsx(),
       // Docs: https://github.com/vuetifyjs/vuetify-loader
-      vuetifyPlugin({
+      VuetifyPlugin({
         autoImport: true
       }),
       // Docs: https://github.com/gxmari007/vite-plugin-eslint
-      eslintPlugin(),
+      EslintPlugin(),
       // Docs: https://github.com/electron-vite/vite-plugin-electron
-      electronPlugin([
+      ElectronPlugin([
         {
           entry: ['src/main/index.ts'],
           onstart: (options) => {
@@ -71,7 +71,7 @@ export default defineConfig(() => {
           }
         }
       ]),
-      rendererPlugin()
+      RendererPlugin()
     ]
   }
 })
