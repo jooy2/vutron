8b3aba3e226aacc1ac8f258a858ea5bb71410f6f
leejooy96 <leejooy96@gmail.com>
2023-11-01 09:59:07 +0900

    test: reconfigure test for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  outputDir: 'tests/results'
+  outputDir: 'tests/results',
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  timeout: 60000,
+  expect: {
+    timeout: 10000
+  }
 })
