d07d86e8598c7495d7aa7a769f77b7a21acf1fe5
leejooy96 <leejooy96@gmail.com>
2025-03-20 10:44:30 +0900

    refactor: ignore install extension when failed to install

--- src/main/index.dev.ts
+++ src/main/index.dev.ts
@@ -1,10 +1,22 @@
 // Warning: This file is only used in the development environment
 // and is removed at build time.
 // Do not edit the file unless necessary.
-import { installExtension, VUEJS_DEVTOOLS } from 'electron-extension-installer'
+import { installExtension } from 'electron-extension-installer'
+import { app } from 'electron'
 
-installExtension(VUEJS_DEVTOOLS, {
-  loadExtensionOptions: {
-    allowFileAccess: true
-  }
-})
+const vue3DevTools = {
+  id: 'nhdogjmejiglipccpnnnanhbledajbpd',
+  version: '7.7.0'
+}
+
+try {
+  app.on('ready', async () => {
+    await installExtension(vue3DevTools, {
+      loadExtensionOptions: {
+        allowFileAccess: true
+      }
+    })
+  })
+} catch {
+  // Do nothing
+}
