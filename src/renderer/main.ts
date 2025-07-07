a0e16e4c2d058e103f38670aa76d4cf16b602f7b
leejooy96 <leejooy96@gmail.com>
2025-07-07 16:15:40 +0900

    refactor: create pinia from external file

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from '@/renderer/App.vue'
 import router from '@/renderer/router'
 import vuetify from '@/renderer/plugins/vuetify'
 import i18n from '@/renderer/plugins/i18n'
+import pinia from '@/renderer/plugins/pinia'
 
 // Add API key defined in contextBridge to window object type
 declare global {
@@ -15,6 +15,6 @@ declare global {
 
 const app = createApp(App)
 
-app.use(vuetify).use(i18n).use(router).use(createPinia())
+app.use(vuetify).use(i18n).use(router).use(pinia)
 
 app.mount('#app')
