0e8d3044275c271d1b1029dcd5dd4c70b11dace9
leejooy96 <leejooy96@gmail.com>
2025-09-03 19:29:02 +0900

    chore: detect typescript no-unused-vars lint rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -71,6 +71,14 @@ export default pluginTypeScriptESLint.config(
           }
         }
       ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
       '@typescript-eslint/no-explicit-any': 'off'
     }
   }
