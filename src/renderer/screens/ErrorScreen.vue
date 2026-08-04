6a53a393fea5c47e36987d3a91df7037f053cbce
leejooy96 <leejooy96@gmail.com>
2026-08-04 09:39:10 +0900

    feat: localize the error screen instead of hardcoding English

--- src/renderer/screens/ErrorScreen.vue
+++ src/renderer/screens/ErrorScreen.vue
@@ -1,3 +1,32 @@
+<script setup lang="tsx">
+import { useI18n } from 'vue-i18n'
+import { mdiAlertCircleOutline } from '@mdi/js'
+
+const { t } = useI18n()
+</script>
+
 <template>
-  <div>Unknown error occurred</div>
+  <v-container>
+    <v-row
+      no-gutters
+      class="text-center"
+    >
+      <v-col cols="12">
+        <v-icon
+          :icon="mdiAlertCircleOutline"
+          size="150"
+          color="error"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        class="my-4"
+      >
+        <h2>{{ t('title.error') }}</h2>
+        <p class="my-2">
+          {{ t('desc.error-desc') }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
